54dd2e6c75ba84bb6dbec2cbd134f033ccb2d79c
Touhidul Alam Seyam <126227356+Seyamalam@users.noreply.github.com>
2024-03-20 20:18:43 +0000

    Update requirements.txt and package.json, add image prediction functionality to index.py

--- api/index.py
+++ api/index.py
@@ -1,7 +1,44 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+import numpy as np
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
 
 app = FastAPI()
 
+model_path = "api/tomato.h5"
+
+MODEL = tf.keras.models.load_model(model_path)
+
+#MODEL = tf.saved_model.load("/workspaces/Leaf_Disease_App/api/Custom")
+
+
+TOMATO_CLASS_NAMES = ["Tomato___Bacterial_spot", "Tomato___Early_blight", "Tomato___Late_blight", "Tomato___Leaf_Mold", "Tomato___Septoria_leaf_spot", "Tomato___Spider_mites Two-spotted_spider_mite", "Tomato___Target_Spot", "Tomato___Tomato_Yellow_Leaf_Curl_Virus","Tomato___Tomato_mosaic_virus", "Tomato___healthy"]
+
+
 @app.get("/api/python")
 def hello_world():
-    return {"message": "Hello World"}
+    return {"message": "Hello Worl"}
+
+def read_file_as_image(data) -> np.ndarray:
+    image = np.array(Image.open(BytesIO(data)))
+    return image
+
+
+@app.post("/predict_tomato")
+async def predict(
+    file: UploadFile = File(...)
+):
+    image = read_file_as_image(await file.read())
+    img_batch = np.expand_dims(image, 0)
+    
+    predictions = MODEL.predict(img_batch)
+
+    predicted_class = TOMATO_CLASS_NAMES[np.argmax(predictions[0])]
+    confidence = np.max(predictions[0])
+    return {
+        'class': predicted_class.replace("_", " "),
+        'confidence': float(confidence)
+    }
